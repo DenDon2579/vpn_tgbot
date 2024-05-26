150f5e444276a357c69d4a785e3994ccb459c1bd
unknown <DenDon2579>
2024-05-26 15:25:05 +0300

    fixes

--- src/mongo/index.ts
+++ src/mongo/index.ts
@@ -1,6 +1,7 @@
 import { MongoClient, ServerApiVersion } from 'mongodb';
-const uri =
-  'mongodb+srv://sososka2804:PkwUuhX1m7FYglkZ@cluster0.cwkfhyr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+import dotenv from 'dotenv'
+dotenv.config()
+const uri = process.env.MONGO || ''
 
 const mongo = new MongoClient(uri, {
   serverApi: {
--- src/tgBot/pages/subscription/subscriptionPayPage.ts
+++ src/tgBot/pages/subscription/subscriptionPayPage.ts
@@ -3,6 +3,8 @@ import { IKeyboardEventPayload } from '../../../types/bot';
 import { subscriptionCost } from '../../../params';
 import generatePaymentCode from '../../../utils/generatePaymentCode';
 import { bot } from '../..';
+import { mongoDB } from '../../../mongo';
+import { DateTime } from 'luxon';
 
 export default async ({
   msgId,
@@ -10,15 +12,29 @@ export default async ({
   userName,
   custom: days,
 }: IKeyboardEventPayload) => {
+  const isTrial = days === 'trial';
+
+  if (isTrial) {
+    const seconds = DateTime.now().plus({ hours: 3 }).toUnixInteger();
+    await mongoDB
+      .collection('users')
+      .updateOne(
+        { telegramID: userId },
+        { $set: { subscriptionExpiresAt: seconds } }
+      );
+  }
+
   const paymentCode = generatePaymentCode();
   const subCost = subscriptionCost[days as keyof typeof subscriptionCost];
-  const message = `<b>Ne4Net - Оплата подписки на <i>${days} дней</i>:</b>
+  const message = !isTrial
+    ? `<b>Ne4Net - Оплата подписки на <i>${days} дней</i>:</b>
 --------------------------------------------------------------------
 Переведите <b>${subCost}RUB</b> через <b>СБП 🇷🇺</b>\n
 Номер: <code>+79897178319</code>
 Банк: <b>Тинькофф</b>\n
 В комментарии к переводу <b>ОБЯЗАТЕЛЬНО</b> укажите код: <code>${paymentCode}</code>\n
-Подписка будет оформлена в течение 24 часов после оплаты`;
+Подписка будет оформлена в течение 24 часов после оплаты`
+    : `<b>✅ Пробная подписка оформлена<b/>`;
 
   await bot.editMessageText(message, {
     parse_mode: 'HTML',
@@ -26,13 +42,17 @@ export default async ({
     message_id: msgId,
     reply_markup: {
       inline_keyboard: [
-        [
-          {
-            text: '🚀 Перевел(а) 🚀',
-            callback_data: `page$subscriptionRequestOk$${days}/${subCost}/${paymentCode}`,
-          },
-        ],
-        [{ text: 'Назад', callback_data: 'page$subscriptionBuy' }],
+        !isTrial
+          ? [
+              {
+                text: '🚀 Перевел(а) 🚀',
+                callback_data: `page$subscriptionRequestOk$${days}/${subCost}/${paymentCode}`,
+              },
+            ]
+          : [],
+        !isTrial
+          ? [{ text: 'Назад', callback_data: 'page$subscriptionBuy' }]
+          : [{ text: '🏠 На главную 🏠', callback_data: 'page$home' }],
       ],
     },
   });

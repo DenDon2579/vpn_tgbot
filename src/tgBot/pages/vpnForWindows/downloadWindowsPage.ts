a33f608dbced4dd7edda8afaf37f2cb3cf715cba
DenDon2579 <sososka2804@gmail.com>
2024-06-05 22:52:42 +0300

    fixes

--- src/tgBot/pages/vpnForWindows/downloadWindowsPage.ts
+++ src/tgBot/pages/vpnForWindows/downloadWindowsPage.ts
@@ -8,18 +8,10 @@ export default async ({
   userName,
   custom: isFirstTime,
 }: IKeyboardEventPayload) => {
-  // await bot.editMessageText(message, {
-  //   parse_mode: 'HTML',
-  //   chat_id: userId,
-  //   message_id: msgId,
-  //   reply_markup: {
-  //     inline_keyboard: [
-  //       [{ text: 'Назад', callback_data: 'page$vpnForWindows' }],
-  //     ],
-  //   },
+  try {
+    await bot.deleteMessage(userId, msgId);
+  } catch (e) {}
 
-  await bot.deleteMessage(userId, msgId);
-  // });
   await bot.sendDocument(userId, SETUP_ID, {
     caption: '<b>Ваш установочный файл</b>',
     parse_mode: 'HTML',
